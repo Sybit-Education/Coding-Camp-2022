5ff93b5dd4c92bce0e37f286a6dfd7a0df7a420a
Schulung <Schulung@schulungsnb03.sybit.de>
2022-08-03 11:07:40 +0200

    update bild

--- src/views/UeberUns.vue
+++ src/views/UeberUns.vue
@@ -1,9 +1,18 @@
 <template>
     <h1> Über uns </h1>
-    <img src=""/>
+    <div class="gruppenbild">
+    <img src="https://lh3.googleusercontent.com/pw/AL9nZEXpU-XxkCZn0_U4y3u6L1P3GajNodVuq9f_QxNgdNsxnV2XBcglGvTlCmW0j8qhEbagy0Fsfqvb5-vTG-UuK4Xeg1s98uFmIkIZxFAkN48MH7N_AWrRvQgELlMqz6bjVnpd4PUaLi022zCT-KUBXgRn8w=w1215-h911-no?authuser=0" class="gruppenbild"/>
+    </div>
     <br>Wir haben diese Web-App im Rahmen des einwöchigen Coding-Camps welches die Sybit GmbH jährlich veranstaltet entworfen, entwickelt und programmiert.
     <br>Der Gedanke dahinter war, dass wir das Ferienprogramm für Kinder der Stadt Radolfzell, welches bisher analog als Heftchen vorlag, digitalisieren und so noch mehr Kinder, Jugendliche aber auch Eltern erreicht werden.
     <p>Durch die Woche konnte unser 10-köpfiges Team, welches aus Schülern verschiedenen Alters bestand, einen Einblick in den Alltag des Ausbildungsberufes des Fachinformatikers für Anwendungsentwicklung erhalten.</p>
     <p>Dabei halfen uns die Auszubildenden, die in eben diesem Beruf ihre Ausbildung absolvieren, da wir in einer Woche natürlich nicht die Programmiersprache die wir nutzten erlernen konnten.</p>
     <p>Wir hoffen, dass Ihnen unsere Web-App gefällt! Viel Spaß weiterhin auf unseren Seiten!</p>
 </template>
+
+<style>
+.gruppenbild{
+    height: 250px;
+    text-align: center;
+}
+</style>
