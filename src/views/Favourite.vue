96cca7921896781fe11300004ef464fd0ea340d0
Schulungs NB 02 <schulungsnb02@sybit.de>
2022-08-03 09:35:07 +0200

    Favourite Seite angelegt

---
+++ src/views/Favourite.vue
@@ -0,0 +1,32 @@
+<template>
+  <search @search="eventList = $event" />
+  <event-list v-if="eventList.length" :list="eventList" />
+  <n-alert v-else title="Keine Angebote gefunden!" type="warning" />
+</template>
+
+<script>
+import { useEventStore } from '../store/event.store'
+import EventList from '../components/event/List.vue'
+import Search from '../components/Search.vue'
+export default {
+  name: 'Favourite',
+  components: { EventList, Search },
+  data () {
+    return {
+      // TODO: Data from Airtable
+      eventList: [],
+      searchInput: ''
+    }
+  },
+  created () {
+    const eventStore = useEventStore()
+    eventStore.fetchEventRecords().then(list => {
+      this.eventList = list
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
