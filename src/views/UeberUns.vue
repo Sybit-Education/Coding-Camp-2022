a3b0ee479af18f1595ff2c1f7d909b3da2c361b4
EricZeller <zellereric2@gmail.com>
2022-08-03 10:36:39 +0200

    ueberuns schwafeltext

--- src/views/UeberUns.vue
+++ src/views/UeberUns.vue
@@ -1,4 +1,9 @@
 <template>
     <h1> Über uns </h1>
     <img src=""/>
+    <br>Wir haben diese Web-App im Rahmen des einwöchigen Coding-Camps welches die Sybit GmbH jährlich veranstaltet entworfen, entwickelt und programmiert.
+    <br>Der Gedanke dahinter war, dass wir das Ferienprogramm für Kinder der Stadt Radolfzell, welches bisher analog als Heftchen vorlag, digitalisieren und so noch mehr Kinder, Jugendliche aber auch Eltern erreicht werden.
+    <p>Durch die Woche konnte unser 10-köpfiges Team, welches aus Schülern verschiedenen Alters bestand, einen Einblick in den Alltag des Ausbildungsberufes des Fachinformatikers für Anwendungsentwicklung erhalten.</p>
+    <p>Dabei halfen uns die Auszubildenden, die in eben diesem Beruf ihre Ausbildung absolvieren, da wir in einer Woche natürlich nicht die Programmiersprache die wir nutzten erlernen konnten.</p>
+    <p>Wir hoffen, dass Ihnen unsere Web-App gefällt! Viel Spaß weiterhin auf unseren Seiten!</p>
 </template>
