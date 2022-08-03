e3bb8f5414d669eb6b2b5ec59532f6b152527942
Schulung <schulung@sybit.de>
2022-08-03 11:30:04 +0200

    filter-price

--- src/components/filter/Filter.vue
+++ src/components/filter/Filter.vue
@@ -5,21 +5,24 @@
     </n-button>
     <div :style="`margin: 0 0 10px 0; display: ${toggle ? '' : 'none'}`">
       <category-filter @update="filter.categories = $event; filterEvents()" />
+      <price-filter @update="filter.price = $event; filterEvents()"/>
     </div>
 </template>
 
 <script>
 import { useEventStore } from '../../store/event.store'
 import CategoryFilter from './Category.vue'
+import PriceFilter from './Price.vue'
 export default {
-  components: { CategoryFilter },
+  components: { CategoryFilter, PriceFilter },
   name: 'Filter',
   data () {
     return {
       toggle: false,
       eventList: [],
       filter: {
-        categories: []
+        categories: [],
+        price: []
       }
     }
   },
@@ -31,8 +34,11 @@ export default {
   },
   methods: {
     filterEvents () {
-      const list = this.filterByCategory(this.eventList)
+      const list = this.filterByCategory(this.filterByPrice(this.eventList))
       this.$emit('filter', list)
+    },
+    filterValues (x, min, max) {
+      return x >= min && x <= max
     },
     filterByCategory (events) {
       if (this.filter.categories && this.filter.categories.length !== 0) {
@@ -51,6 +57,19 @@ export default {
         return list
       } else {
         return events
+      }
+    },
+    filterByPrice (events) {
+      const range = this.filter.price
+      if (range?.length === 2) {
+        if (range[0] > range[1]) {
+          const temp = range[0]
+          range[1] = range[0]
+          range[0] = temp
+        }
+        return events.filter(event => (this.filterValues(event.price, range[0], range[1])) ? event : '')
+      } else {
+        return (events)
       }
     }
   }
