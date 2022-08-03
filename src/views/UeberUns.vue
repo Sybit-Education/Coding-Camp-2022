361647c9246e8d2062b10484c347e949ea7fac45
EricZeller <zellereric2@gmail.com>
2022-08-03 12:06:34 +0200

    update text

--- src/views/UeberUns.vue
+++ src/views/UeberUns.vue
@@ -1,23 +1,23 @@
 <template>
-    <h1> Über uns </h1>
-    <h2> Coding Camp 2022 hi</h2>
-    <div class="gruppenbild">
+<h1> Über uns </h1>
+   <h2>Sybit Coding-Camp</h2>
+   Das Sybit Coding-Camp findet jährlich statt und bietet Jugendlichen die Chance, einen Einblick in den Beruf von Softwareentwicklerinnen und Softwareentwicklern, sowie die Ausbildung als Fachinformatiker:in (Anwendungsentwicklung) zu erhalten. Die Auszubildenden des Unternehmens bereiten hierfür im Vorfeld ein Projekt vor und coachen die Jugendlichen während des Coding-Camps.
+   <h2>Was haben wir denn während des Coding-Camps gemacht?</h2>
+   Während des Coding-Camps haben wir eine Web-App für das Radolfzeller Ferienprogramm gecodet. Wir sind insgesamt zehn Schüler und sechs Auszubildende gewesen. Die Auszubildenden haben uns vieles über das Programmieren mit Vue, JavaScript, HTML und CSS beigebracht. Wir haben uns in fünf Gruppen aufgeteilt und an den zuvor besprochenen Aufgaben gearbeitet. Uns wurde erklärt, wie eine Firma wie Sybit arbeitet. Danach haben uns das Sybit-Team die Personas erklärt und vorgestellt. Gegen Ende unserer Vorbereitung haben wir vier Personas erstellt und jeweils User Storys für diese erstellt.
+ <p></p>
+ <p></p>
+ <p></p>
+ <div class="gruppenbild">
     <img src="https://lh3.googleusercontent.com/pw/AL9nZEXpU-XxkCZn0_U4y3u6L1P3GajNodVuq9f_QxNgdNsxnV2XBcglGvTlCmW0j8qhEbagy0Fsfqvb5-vTG-UuK4Xeg1s98uFmIkIZxFAkN48MH7N_AWrRvQgELlMqz6bjVnpd4PUaLi022zCT-KUBXgRn8w=w1215-h911-no?authuser=0" class="gruppenbild"/>
-    </div>
-    <br>Wir haben diese Web-App im Rahmen des einwöchigen Coding-Camps welches die Sybit GmbH jährlich veranstaltet entworfen, entwickelt und programmiert.
-    <br>Der Gedanke dahinter war, dass wir das Ferienprogramm für Kinder der Stadt Radolfzell, welches bisher analog als Heftchen vorlag, digitalisieren und so noch mehr Kinder, Jugendliche aber auch Eltern erreicht werden.
-    <p>Durch die Woche konnte unser 10-köpfiges Team, welches aus Schülern verschiedenen Alters bestand, einen Einblick in den Alltag des Ausbildungsberufes des Fachinformatikers für Anwendungsentwicklung erhalten.</p>
-    <p>Dabei halfen uns die Auszubildenden, die in eben diesem Beruf ihre Ausbildung absolvieren, da wir in einer Woche natürlich nicht die Programmiersprache die wir nutzten erlernen konnten.</p>
-    <p>Wir hoffen, dass Ihnen unsere Web-App gefällt! Viel Spaß weiterhin auf unseren Seiten!</p>
-
+</div>
 </template>
 
 <style>
-img{
-    max-width: 100%;
-    height: auto;
-    width: auto/9; /* ie8 */
-    img-align: center;
+.gruppenbild{
+    text-align: center;
+    width: 90%;
+    max-width: 900px;
+    margin-left: auto;
+    margin-right: auto;
 }
-
 </style>
