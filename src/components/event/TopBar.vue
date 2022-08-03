974a15a478b61bd4514f4d785d1e406c6f1f7b8a
Schulung <Schulung@schulungsnb03.sybit.de>
2022-08-03 15:07:51 +0200

    BackBtn to TopBar

--- src/components/event/TopBar.vue
+++ src/components/event/TopBar.vue
@@ -1,28 +1,19 @@
 <template>
   <n-grid cols="2 m:2 l:2" responsive="screen" class="top-bar">
-    <n-gi
-      ><vue-feather
-        class="back"
-        type="arrow-left"
-        size="30"
-        @click="
-          this.$router.push({
-            name: 'Home'
-          })
-        "
-    /></n-gi>
+    <n-gi>
+      <BackBtn/>
+    </n-gi>
     <n-gi class="share"><vue-feather type="share-2" size="30" /></n-gi>
   </n-grid>
 </template>
 <script>
+import BackBtn from './details/BackBtn.vue'
 export default {
-  name: 'EventTopBar'
+  name: 'EventTopBar',
+  components: { BackBtn }
 }
 </script>
 <style scoped>
-.back {
-  cursor: pointer;
-}
 .share {
   cursor: pointer;
   position: absolute;
