473f303cbf8db7fe23f916223cc67d1e3410f32a
Schulungs NB 02 <schulungsnb02@sybit.de>
2022-08-04 14:59:31 +0200

    added markdown wrapper

---
+++ src/components/MarkdownWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+    <vue-markdown linkify typographer>{{ source }}</vue-markdown>
+</template>
+
+<script>
+import VueMarkdown from '@adapttive/vue-markdown'
+
+export default {
+  name: 'MarkdownWrapper',
+  props: {
+    source: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    VueMarkdown
+  }
+}
+
+</script>
--- src/components/event/details/Description.vue
+++ src/components/event/details/Description.vue
@@ -1,13 +1,21 @@
 <template>
-  <span class="event-title" v-if="event.description">{{ event.description }}</span>
+    <span class="event-title" v-if="event.description">
+      <markdown-wrapper :source="event.description"/>
+    </span>
 </template>
+
 <script>
+import MarkdownWrapper from '@/components/MarkdownWrapper.vue'
+
 export default {
   name: 'EventDescription',
   props: {
     event: {
       required: true
     }
+  },
+  components: {
+    MarkdownWrapper
   }
 }
 </script>
