1f1edfcec60591f5a130aaa84957388ad6e882d8
Gideon Godejohann <110403075+AmariEldar@users.noreply.github.com>
2022-08-04 14:21:25 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
-<h2 style="text-align:center">Ferienprogramm Radolfzell</h2>
+  <h2 style="text-align: center">Ferienprogramm Radolfzell</h2>
   <n-grid :x-gap="10" cols="12">
     <n-gi :span="10">
       <search @search="eventList = $event" />
     </n-gi>
     <n-gi :span="2">
-      <n-button @click="this.$router.push({name:'Favoriten'})" size="medium" style="margin-top: 15px">
-        <vue-feather stroke="black" fill="#f0a020" size="30" type="star" />
+      <n-button
+        @click="this.$router.push({ name: 'Favoriten' })"
+        size="medium"
+        style="margin-top: 15px"
+      >
+        <n-badge :value="favoriteListSize" :max="15">
+          <vue-feather stroke="black" fill="#f0a020" size="30" type="star" />
+        </n-badge>
       </n-button>
     </n-gi>
   </n-grid>
@@ -17,9 +23,10 @@
 
 <script>
 import EventList from '../components/event/List.vue'
-import Search from '../components/Search.vue'
 import Filter from '../components/filter/Filter.vue'
+import Search from '../components/Search.vue'
 import { useEventStore } from '../store/event.store'
+import { useFavoriteStore } from '../store/favorite.store'
 export default {
   name: 'Home',
   components: { EventList, Search, Filter },
@@ -27,10 +34,13 @@ export default {
     return {
       // TODO: Data from Airtable
       eventList: [],
-      searchInput: ''
+      searchInput: '',
+      favouriteListSize: 0
     }
   },
   created () {
+    const favoriteStore = useFavoriteStore()
+    this.favoriteListSize = favoriteStore.getFavoriteList.length
     const eventStore = useEventStore()
     eventStore.fetchEventRecords().then((list) => {
       this.eventList = list
