74652a48cf03179d7a0b46d6320bdebbaafe87f0
Jan Steppacher <jan.steppacher@sybit.de>
2022-08-05 13:21:27 +0200

    changed sw place

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: 'black',
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: 'src/registerServiceWorker.js'
+      swSrc: 'public/registerServiceWorker.js'
     },
     manifestOptions: {
       // TODO: App Icons?
